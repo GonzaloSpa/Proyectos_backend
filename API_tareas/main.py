a34a8ef0a2c280bcee9ae9c98789ade6706df1e9
GonzaloSpa <gonzalospaltro98@gmail.com>
2024-03-10 21:22:39 -0300

    backend 1.0 terminado

--- API_tareas/main.py
+++ API_tareas/main.py
@@ -1,8 +1,61 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException,status
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
+class Tareas(BaseModel):
+    id: Optional[int] # el id opcional no me funciono por eso se implemento con la variable ultimo_id, incrementa el numero de id sin importar que escribamos en el body JSON
+    tarea: str
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+db_fake = []
+# Tareas(id=0, tarea="Hacer compras del mes"),
+# Tareas(id=1, tarea="Corregir detalles de la reunion") con tareas o en este caso sin las tareas 
+
+ultimo_id = len(db_fake)
+
+# listando las tareas
+@app.get("/tarea/", status_code=200)
+async def lista():
+    if len(db_fake) == 0:
+        return {"No tenes tareas agregadas"}
+    
+    return db_fake # retorna la db fake con toda la lista actual
+
+
+# agregando tareas 
+@app.post("/tarea/", response_model=Tareas, status_code=201)
+async def agregarTarea(tarea: Tareas):
+    global ultimo_id
+    ultimo_id +=1
+    tarea.id = ultimo_id 
+    db_fake.append(tarea)
+    return tarea
+    
+    
+# actualizando tareas segun id 
+
+@app.put("/tarea/")
+async def actualizarTarea(tarea: Tareas):
+    found = False
+
+    for index, saved_user in enumerate(db_fake):
+        if saved_user.id == tarea.id:
+            db_fake[index] = tarea
+            found = True
+    if not found:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="error, el id no existe")
+    else:      
+        return tarea
+    
+# Borrando tareas por id 
+
+@app.delete("/tarea/{id}")        # usando query
+async def borrarTarea(id: int):
+    found = False
+    for index, saved_user in enumerate(db_fake):
+        if saved_user.id == id:
+           del db_fake[index]
+           found = True           
+    if not found:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="error, no se ha podido eliminar la tarea") 
